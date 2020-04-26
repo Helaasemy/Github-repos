0c1f6094154cbf1ef10c90b1fb88a3bed283bb04
hamza Elaasemy <hamza.elaasemy@imedia24.de>
2020-04-26 18:24:18 +0000

    initializing project and creating first component

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Repository from './Repository/Repository';
+
+function App() {
+  return (
+    <>
+    <Repository/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Repository/Repository.js
@@ -0,0 +1,12 @@
+
+import * as React from 'react';
+
+const Repository = () => {
+
+  return (
+    <>
+    <p>Initialazing the project</p>
+    </>
+  );
+};
+export default Repository;
