e98745eb3a6826fe80ec838a06fd8a0fb6b4ab6b
hamza Elaasemy <hamza.elaasemy@imedia24.de>
2020-04-30 12:15:53 +0000

    styling

--- src/Github/index.js
+++ src/Github/index.js
@@ -2,17 +2,16 @@ import sub from 'date-fns/sub';
 import format from 'date-fns/format';
 import axios from 'axios';
 
-export const fetchRepos = (page = 0) =>{
+export const fetchRepos = (page = 0) => {
     let result = sub(new Date(), { days: 30 })
     result = format(result, 'yyyy-MM-dd')
     let API_URL = `https://api.github.com/search/repositories?q=created:>${result}&sort=stars&order=desc&page=${page}
     `;
     return axios
-    .get(API_URL)
-    .then(repos => {
-        const repositories = repos.data.items;
-        return repositories
-    })
-
+        .get(API_URL)
+        .then(repos => {
+            const repositories = repos.data.items;
+            return repositories
+        })
 }
 
--- src/components/Repository/Repository.js
+++ src/components/Repository/Repository.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import { Row, Col, Skeleton, Card, Badge } from 'antd';
 import formatDistance from 'date-fns/formatDistance';
-import debounce from 'lodash.debounce'
-import { fetchRepos } from "../../Github";
+import debounce from 'lodash.debounce';
+import { fetchRepos } from '../../Github';
+import './Repository.css';
 
 class Repository extends Component {
     constructor(props) {
@@ -35,12 +36,8 @@ class Repository extends Component {
     }
 
     loadMore() {
-        console.log("daz mn hna: ");
-
         if (window.innerHeight + document.documentElement.scrollTop === document.scrollingElement.scrollHeight) {
             this.setState({ currentPage: this.state.currentPage + 1 }, () => {
-                console.log("Current Page : ", this.state.currentPage);
-
                 fetchRepos(this.state.currentPage)
                     .then(repositories => {
                         this.setState({ repositories: [...this.state.repositories, ...repositories] });
@@ -51,31 +48,36 @@ class Repository extends Component {
 
     render() {
         if (this.state.loading) {
-            return <Skeleton />;
+            return <Row gutter={16} >
+                <Col span={16} className="row">
+                    <Skeleton avatar paragraph={{ rows: 4 }} />
+                </Col>
+            </Row>;
         } else {
             return (
                 <>
-                    <Row gutter={16}>
-                        <Col span={6}>
-                            {/* <img
-                                src={`https://cdn.iconscout.com/icon/free/png-512/avatar-380-456332.png`}
-                                alt="test"
-                            /> */}
-                        </Col>
-                        <Col span={6}>
+                    <Row gutter={16} >
+                        <Col span={16} className="row">
                             {this.state.repositories.map(repo =>
                                 <div key={repo.id} className="site-card-border-less-wrapper">
-                                    <Card title={repo.name} bordered={true} style={{ width: 600 }}>
+                                    <Col >
+                                        <img
+                                            className="avatar"
+                                            src={`${repo.owner.avatar_url}`}
+                                            alt="test"
+                                        />
+                                    </Col>
+                                    <Card title={repo.name} bordered={true} className="card">
                                         <p>{repo.description}</p>
                                         <Row>
                                             <Col >
-                                                <Badge className="site-badge-count-109" count={`stars : ${repo.stargazers_count}`} style={{ backgroundColor: '#52c41a' }} />
+                                                <Badge className="site-badge-count-109" count={`stars : ${repo.stargazers_count} `} style={{ backgroundColor: '#52c41a' }} />
                                             </Col>
                                             <Col >
-                                                <Badge className="site-badge-count-109" count={`stars : ${repo.open_issues_count}`} style={{ backgroundColor: '#52c41a' }} />
+                                                <Badge className="site-badge-count-109" count={`  Issues : ${repo.open_issues_count}`} style={{ backgroundColor: '#52c41a' }} />
                                             </Col>
                                             <Col >
-                                                <p>Submitted  {formatDistance(new Date(), new Date(repo.created_at))} by {repo.owner.login}</p>
+                                                <p>Submitted  {formatDistance(new Date(), new Date(repo.created_at))} ago by {repo.owner.login}</p>
                                             </Col>
                                         </Row>
 
